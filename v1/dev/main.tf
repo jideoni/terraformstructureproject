58f75fc89b3e68baa4157ee23ea8226ef7a6fd0f
jideoni <onibabajide34@gmail.com>
2025-08-26 14:12:09 -0500

    created vpc in main.tf

--- v1/dev/main.tf
+++ v1/dev/main.tf
@@ -1,7 +1,7 @@
 # Configure the AWS Provider
 provider "aws" {
   profile = "terraform-admin" #role or user or user group
-  region  = "us-east-1"
+  region  = "us-east-2"
 }
 
 terraform {
@@ -12,5 +12,16 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_support   = false
+  enable_dns_hostnames = false
+
+  tags = {
+    Name = "dev-main"
   }
 }
