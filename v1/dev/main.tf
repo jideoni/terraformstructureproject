6a3d32c80c05400b952deb393b9417a9a7d338e2
jideoni <onibabajide34@gmail.com>
2025-08-26 14:27:42 -0500

    added variable.tf, provider.tf

--- v1/dev/main.tf
+++ v1/dev/main.tf
@@ -1,27 +1,10 @@
-# Configure the AWS Provider
-provider "aws" {
-  profile = "terraform-admin" #role or user or user group
-  region  = "us-east-2"
-}
-
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.cidr_block
 
   enable_dns_support   = false
   enable_dns_hostnames = false
 
   tags = {
-    Name = "dev-main"
+    Name = "${var.env}-main"
   }
 }
